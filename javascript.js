8c4e9f86592a01fd1fce4c0dea7f76ddf7f915ee
DarkBlueButterfly <208701196+DarkBlueButterfly@users.noreply.github.com>
2025-06-02 16:29:53 -0600

    Add basic math functions
    Added functions for basic math operations and the operate function.

---
+++ javascript.js
@@ -0,0 +1,36 @@
+// basic math functions:
+// add:
+function add(a, b) {
+    return a + b;
+}
+
+// subtract
+function subtract(a, b) {
+    return a - b;
+}
+
+// multiply
+function multiply(a, b) {
+    return a * b;
+}
+
+// divide
+function divide(a, b) {
+    return a / b;
+}
+
+let var1 = 0;
+let var2 = 0;
+let operator = "symbol";
+
+function operate(operator, var1, var2) {
+    if(operator === `+` ) {
+        return add(var1, var2);
+    } else if (operator === `-`) {
+        return subtract(var1, var2);
+    } else if (operator === `*`) {
+        return multiply(var1, var2);
+    } else if (operator === `/`) {
+        return divide(var1, var2);
+    }
+}
