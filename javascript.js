44e4a7a449d660e545d6b68f3dac2893d975b4e1
DarkBlueButterfly <208701196+DarkBlueButterfly@users.noreply.github.com>
2025-06-03 20:49:20 -0600

    Add working button function for numbers
    Button presses display respective numbers on calculator display.
    Updated index.html with classes and ids.

--- javascript.js
+++ javascript.js
@@ -19,9 +19,9 @@ function divide(a, b) {
     return a / b;
 }
 
-let var1 = 0;
-let var2 = 0;
-let operator = "symbol";
+// let var1 = 0;
+// let var2 = 0;
+// let operator = "symbol";
 
 function operate(operator, var1, var2) {
     if(operator === `+` ) {
@@ -37,17 +37,58 @@ function operate(operator, var1, var2) {
 
 const display = document.querySelector(".display");
 
-
-function calcDisplay() {
-    display.textContent = `Hello World!`;
+//Calculator Display
+function calcDisplay(num) {
+    variable = num;
+    return display.textContent = `${num}`;
+    // console.log(`${num}`);
 }
 
-calcDisplay()
+// calcDisplay("Hello World!")
 
 function clear() {
     display.textContent = ``;
 }
 
-const clearbtn = document.querySelector(".clear");
-clearbtn.addEventListener("click", clear);
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", clear);
+
+const keypad = document.querySelector("#keypad");
+// const numBtn = keypad.querySelector("button");
+
+// numBtn.addEventListener("click", )
+const one = keypad.querySelector("#one");
+one.addEventListener("click", () => {calcDisplay(1)});
+const two = keypad.querySelector("#two");
+two.addEventListener("click", () => {calcDisplay(2)});
+const three = keypad.querySelector("#three");
+three.addEventListener("click", () => {calcDisplay(3)});
+const four = keypad.querySelector("#four");
+four.addEventListener("click", () => {calcDisplay(4)});
+const five = keypad.querySelector("#five");
+five.addEventListener("click", () => {calcDisplay(5)});
+const six = keypad.querySelector("#six");
+six.addEventListener("click", () => {calcDisplay(6)});
+const seven = keypad.querySelector("#seven");
+seven.addEventListener("click", () => {calcDisplay(7)});
+const eight = keypad.querySelector("#eight");
+eight.addEventListener("click", () => {calcDisplay(8)});
+const nine = keypad.querySelector("#nine");
+nine.addEventListener("click", () => {calcDisplay(9)});
+const zero = keypad.querySelector("#zero");
+zero.addEventListener("click", () => {calcDisplay(0)});
+
+function storeNum1() {
+    let a = var1;
+    
+    return operate(op, a, b)
+}
+
+const operator = keypad.querySelectorAll("#operator")
+function operate() {
+    if (operator == "/") {
+
+    }
+
+}
 
