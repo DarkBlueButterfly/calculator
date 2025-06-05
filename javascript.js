9cbf8f3e104c9bb339a8515bb49e7154805c8b2f
DarkBlueButterfly <208701196+DarkBlueButterfly@users.noreply.github.com>
2025-06-04 20:11:11 -0600

    Add operator functions
    Try to get calculate function to work properly.

--- javascript.js
+++ javascript.js
@@ -23,17 +23,18 @@ function divide(a, b) {
 // let var2 = 0;
 // let operator = "symbol";
 
-function operate(operator, var1, var2) {
-    if(operator === `+` ) {
-        return add(var1, var2);
-    } else if (operator === `-`) {
-        return subtract(var1, var2);
-    } else if (operator === `*`) {
-        return multiply(var1, var2);
-    } else if (operator === `/`) {
-        return divide(var1, var2);
-    }
-}
+//Rewriting operate function: 
+// function operate(operator, var1, var2) {
+//     if(operator === `+` ) {
+//         return add(var1, var2);
+//     } else if (operator === `-`) {
+//         return subtract(var1, var2);
+//     } else if (operator === `*`) {
+//         return multiply(var1, var2);
+//     } else if (operator === `/`) {
+//         return divide(var1, var2);
+//     }
+// }
 
 const display = document.querySelector(".display");
 
@@ -41,6 +42,8 @@ const keypad = document.querySelector("#keypad");
 const numBtn = keypad.querySelectorAll(".numKey");
 let variable = '';
 let decimalAdded = false;
+let addOperate = false;
+let firstVar = true;
 
 // numBtn.forEach(button => {
 //     button.addEventListener("click", () => {
@@ -53,24 +56,114 @@ let decimalAdded = false;
 //Calculator Display
 function calcDisplay(num) {
     variable += num;
-    return display.textContent = `${variable}`;
-
+    display.textContent = `${variable}`;
+    // return variable; //output... to store function?
+    storeVar(variable); //store function
     // console.log(`${num}`);
 }
+// variable = calcDisplay()
+// console.log(variable)
 
 // Clear button:
 function clear() {
     display.textContent = ``;
     variable = ``;
     decimalAdded = false;
+    // addOperate = false; //add to button click
 }
 const clearBtn = document.querySelector(".clear");
 clearBtn.addEventListener("click", clear);
 
-// const keypad = document.querySelector("#keypad"); //added on line 40
-// const numBtn = keypad.querySelector("button");
+//Store numbers: 
+function storeVar(num) {
+    // let var1 = 0;
+    // let var2 = 0;
+    let newNum = parseFloat(parseFloat(num).toFixed(2)); //changed to numbers
+    if (firstVar === true) {
+        // var1 = newNum;
+        firstVar = false;
+        // return var1; 
+        return newNum;
+    } else {
+        // calculate() //
+        // var2 = newNum;
+        // console.log(var2); //check
+        // return var2;
+        storeVar2(newNum);
+    }
+    // return var1;
+    // calculate(var1, var2); 
+}
 
-// numBtn.addEventListener("click", )
+function storeVar2(num) {
+    let newNum = parseFloat(parseFloat(num).toFixed(2));
+    return newNum;
+}
+
+//Operator function: to store the operators?
+const operators = keypad.querySelectorAll("#operator");
+// function operator() {
+    operators.forEach((operate) => {
+        operate.addEventListener("click", () => {
+            let button = operate.className;
+            // clear the display after the click
+            // display.textContent = ``; 
+            // clear();
+            if (!addOperate) {
+                clear();
+                addOperate = true; //
+            }
+            return button;
+            // console.log(button); //can change your operator?
+            // calculate(); //should be activated when "=" is pressed
+        })
+    })
+    // console.log(button);
+    // if (button === `plus`) { //wait til calculate?
+
+    // }
+// } //end function
+//not right...
+// function operator(variable) {
+//     // let variable = calcDisplay()
+//     let parsed = parseFloat(parseFloat(variable).toFixed(2)); //number not string!
+//     console.log(parsed); //check 
+// }
+
+
+//Calculate "=" 
+const equal = keypad.querySelector("#equal");
+equal.addEventListener("click", () => {
+    calculate();
+    console.log("hi?")
+});
+function calculate() {
+    const a = storeVar();
+    const b = storeVar();
+    const op = operator();
+    let result = 0;
+    // if () {
+        switch (op) {
+            case "add":
+                result = add(a, b);
+                break;
+            case "minus":
+                result = subtract(a, b);
+                break;
+            case "multiply":
+                result = multiply(a, b);
+                break;
+            case "divide":
+                result = divide(a, b);
+                break;
+            default:
+                return;
+        }
+        display.textContent = `${result}`;
+//     }
+}
+
+// Keypad numbers:
 const one = keypad.querySelector("#one");
 one.addEventListener("click", () => {calcDisplay(1)});
 const two = keypad.querySelector("#two");
