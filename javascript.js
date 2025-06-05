6188d3bf61caabc311c52a9c168cb35270b78235
DarkBlueButterfly <208701196+DarkBlueButterfly@users.noreply.github.com>
2025-06-05 11:56:22 -0600

    Code cleanup
    Removed unused code.

--- javascript.js
+++ javascript.js
@@ -19,25 +19,7 @@ function divide(a, b) {
     return a / b;
 }
 
-// let var1 = 0;
-// let var2 = 0;
-// let operator = "symbol";
-
-//Rewriting operate function: 
-// function operate(operator, var1, var2) {
-//     if(operator === `+` ) {
-//         return add(var1, var2);
-//     } else if (operator === `-`) {
-//         return subtract(var1, var2);
-//     } else if (operator === `*`) {
-//         return multiply(var1, var2);
-//     } else if (operator === `/`) {
-//         return divide(var1, var2);
-//     }
-// }
-
 const display = document.querySelector(".display");
-
 const keypad = document.querySelector("#keypad");
 const numBtn = keypad.querySelectorAll(".numKey");
 let variable = '';
@@ -46,67 +28,35 @@ let addOperator = null;
 let firstVar = null;
 let secondVar = null;
 
-// numBtn.forEach(button => {
-//     button.addEventListener("click", () => {
-//         variable += button.textContent;
-//         display.textContent = variable;
-//     })
-// })
-
-
 //Calculator Display
 function calcDisplay(num) {
     variable += num;
     display.textContent = `${variable}`;
-    // return variable; //output... to store function?
     storeVar(variable); //store function
-    // console.log(`${num}`);
 }
-// variable = calcDisplay()
-// console.log(variable)
 
-// Clear button:
+// Reset button:
 function clearAll() {
     display.textContent = ``;
     variable = ``;
     decimalAdded = false;
-    addOperator = null; //add to button click instead...? nvm
+    addOperator = null; 
     firstVar = null;
     secondVar = null;
 }
 const clearBtn = document.querySelector(".clear");
 clearBtn.addEventListener("click", clearAll);
 
-//to not clear everything
+//Clear display
 function clear() {
     display.textContent = ``;
     variable = ``;
     decimalAdded = false;
-    // addOperator = null; //add to button click instead...? nvm
-    // firstVar = null;
-    // secondVar = null;
 }
 
-// Store numbers: is this needed? since there are already variables declared!
-// yes it was needed!
+// Store variables:
 function storeVar(num) {
-    // let var1 = 0;
-    // let var2 = 0;
-    let newNum = parseFloat(parseFloat(num).toFixed(2)); //changed to numbers
-    // if (firstVar === null && addOperator === null) {
-    //     // firstVar = parseFloat(parseFloat(variable).toFixed(2));
-    //     firstVar = newNum;
-    //     // firstVar = false;
-    //     // return var1; 
-    //     return firstVar;  //needed?
-    // } else if(firstVar !== null && addOperator !== null) {
-    //     // calculate() //
-    //     secondVar = newNum;
-    //     // console.log(var2); //check
-    //     return secondVar;
-    //     // storeVar2(newNum); //might not be needed
-    // }
-    //try this! it works!!
+    let newNum = parseFloat(parseFloat(num).toFixed(2)); //converts to number
     if (firstVar !== null && addOperator !== null) {
         secondVar = newNum;
         return secondVar;
@@ -114,62 +64,30 @@ function storeVar(num) {
         firstVar = newNum;
         return firstVar;
     }
-    // return var1;
-    // calculate(var1, var2); 
 }
 
-// function storeVar2(num) {
-//     let newNum = parseFloat(parseFloat(num).toFixed(2));
-//     return newNum;
-// }
-
-//Operator function: to store the operators?
 const operators = keypad.querySelectorAll("#operator");
-// function operator() {
-    operators.forEach((operate) => {
-        operate.addEventListener("click", () => {
-            // let button = operate.className;
-            // addOperator = operate.className; //changed this oops should be in the if
-            // clear the display after the click
-            // display.textContent = ``; 
-            // clear();
-            if (!addOperator) {
-                clear(); //Done - might need to change this so it doesn't reset everything...
-                addOperator = operate.className; //should be here!?
-            }
-            return addOperator;
-            // console.log(button); //can change your operator?
-            // calculate(); //should be activated when "=" is pressed
-        })
+operators.forEach((operate) => {
+    operate.addEventListener("click", () => {
+        if (!addOperator) {
+            clear(); 
+            addOperator = operate.className; 
+        }
+        return addOperator;
     })
-    // console.log(button);
-    // if (button === `plus`) { //wait til calculate?
-
-    // }
-// } //end function
-//not right...
-// function operator(variable) {
-//     // let variable = calcDisplay()
-//     let parsed = parseFloat(parseFloat(variable).toFixed(2)); //number not string!
-//     console.log(parsed); //check 
-// }
-
+})
 
 //Calculate "=" 
 const equal = keypad.querySelector("#equal");
 equal.addEventListener("click", () => {
     calculate();
-    console.log(addOperator) //check
+    // console.log(addOperator) //check
 });
 function calculate() {
-    // const a = storeVar();
-    // const b = storeVar();
-    const a = parseFloat(parseFloat(firstVar).toFixed(2));     //copied
-    const b = parseFloat(parseFloat(secondVar).toFixed(2));    //copied
+    const a = parseFloat(parseFloat(firstVar).toFixed(2));
+    const b = parseFloat(parseFloat(secondVar).toFixed(2));
     const op = addOperator;
-    // const op = operate(); //need to fix
     let result = 0;
-    // if () {
         switch (op) {
             case "add":
                 result = add(a, b);
@@ -188,9 +106,8 @@ function calculate() {
         }
         result = parseFloat(parseFloat(result).toFixed(2));
         display.textContent = `${result}`;
-        console.log(firstVar); //check
-        console.log(secondVar); //check
-//     }
+        // console.log(firstVar); //check
+        // console.log(secondVar); //check
 }
 
 // Keypad numbers:
@@ -222,33 +139,3 @@ decimal.addEventListener("click", () => {
     decimalAdded = true;
     }
 });
-
-// const operator = keypad.querySelector("#operator");
-// operator.addEventListener("click", () => {operate()});
-
-// const divide = keypad.querySelector(".divide");
-// divide.addEventListener("click", () => {calcDisplay(`/`)});
-// const multiply = keypad.querySelector(".multiply");
-// multiply.addEventListener("click", () => {calcDisplay(`*`)});
-// const add = keypad.querySelector(".add");
-// add.addEventListener("click", () => {calcDisplay(`+`)});
-// const minus = keypad.querySelector("#minus");
-// minus.addEventListener("click", () => {calcDisplay(`-`)});
-// const equal = keypad.querySelector("#equal");
-// equal.addEventListener("click", () => {calcDisplay(`=`)});
-
-
-// function storeNum1() {
-//     let a = var1;
-    
-//     return operate(op, a, b)
-// }
-
-// // const operator = keypad.querySelectorAll("#operator")
-// function operate() {
-//     if (operator == "/") {
-
-//     }
-
-// }
-
